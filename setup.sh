fbb2bc9676557c7c96bd1b2fa32929d3cedaf615
Jacob Chio <jlc396@uakron.edu>
2026-01-31 19:29:19 +0000

    init

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-apt-get update
-apt-get install lsb-release software-properties-common gnupg
+# 1. Enable the developer repository (if not already enabled)
+dnf install oracle-linux-developer-release -y
+# 2. Install Ansible
+dnf install ansible git -y
+# 3. Verify installation
+ansible --version
+git clone https://github.com/cisagov/LME.git
+cp ansible.cfg LME/ansible.cfg
+cp lme-environment.env LME/config/lme-environment.env
+cd LME
+./install.sh
+cd ..
+ansible-playbook -i inventory.yaml linuxPlaybook.yaml
