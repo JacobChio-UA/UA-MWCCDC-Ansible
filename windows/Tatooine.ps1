98644b2ea3d2597540424773879fe5a888d38b26
Jake-Charlton <jc481@uakron.edu>
2026-02-12 12:37:37 -0500

    Firewall v1.3

--- windows/Tatooine.ps1
+++ windows/Tatooine.ps1
@@ -4,13 +4,13 @@ function LandOnTat() {
             Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled true
 
             # Set default inbound policy to Block
-            Set-NetFirewallProfile -Profile Domain,Public,Private -DefaultInboundAction Block -DefaultOutboundAction Allow
+            Set-NetFirewallProfile -Profile Domain,Public,Private -DefaultInboundAction Block -DefaultOutboundAction Block
 
-            # Enable logging for dropped packets
+            # Enable logging for packets
             Set-NetFirewallProfile -Profile Domain,Public,Private -LogAllowed true -LogBlocked true
 
             # Disallow Configuration Changes
-            Set=NetFirewallProfile -Profile Domain,Public,Private -AllowLocalPolicyMerge false -AllowLocalIPsecPolicyMerge false -AllowLocalFirewallRules false -AllowLocalIPsecRules false
+            Set-NetFirewallProfile -Profile Domain,Public,Private -AllowLocalPolicyMerge false -AllowLocalIPsecPolicyMerge false -AllowLocalFirewallRules false -AllowLocalIPsecRules false
 
             Write-Host "No Errors in 1st Stage, Continue" 
         }
@@ -22,9 +22,6 @@ function LandOnTat() {
             New-NetFirewallRule -DisplayName "Allow HTTP" -Direction Inbound -Protocol TCP -LocalPort 80 -Action Allow -Profile Domain,Public,Private -Enabled true
             New-NetFirewallRule -DisplayName "Allow HTTPS" -Direction Inbound -Protocol TCP -LocalPort 443 -Action Allow -Profile Domain,Public,Private -Enabled true
 
-            # Block Outbound WinRM
-            New-NetFirewallRule -DisplayName "Block WinRM Outbound" -Direction Outbound -Protocol TCP -LocalPort 5985,5986 -Action Block -Profile Domain,Public,Private -Enabled true
-
             # Remove Preexisting Inbound Rules
             Get-NetFirewallRule -Direction Inbound | Remove-NetFirewallRule
 
@@ -34,13 +31,28 @@ function LandOnTat() {
            Write-Host "2nd Stage Failed"
         }
         try{
-           # Allow Communication with AD / DNS Server
-           New-NetFirewallRule -DisplayName "Allow DNS" -Direction Inbound -Protocol UDP -LocalPort 53 -Action Allow -Profile Domain,Public,Private -Enabled true
-        }
+           # Allow Communication with DNS and AD
+            New-NetFirewallRule -DisplayName "Allow DNS" -Direction Outbound -Protocol UDP -RemotePort 53 -Action Allow -Profile Domain,Public,Private -Enabled true
+            New-NetFirewallRule -DisplayName "Allow LDAP" -Direction Outbound -Protocol TCP -RemotePort 389,636,3268,3269 -Action Allow -Profile Domain,Public,Private -Enabled true
+            New-NetFirewallRule -DisplayName "Allow Kerberos" -Direction Outbound -Protocol TCP -RemotePort 88 -Action Allow -Profile Domain,Public,Private -Enabled true
+            
+            Write-Host "No Errors in 3rd Stage, Continue"
+        }  
         catch {
             Write-Host "3rd Stage Failed"
         }
+        try {
+            # Allow Necessary Connections
+            Enable-NetFirewallRule -DisplayGroup "Core Networking"
+            New-NetFirewallRule -DisplayName "Allow RPC" -Direction Outbound -Protocol TCP -RemotePort 135 -Action Allow -Profile Domain,Public,Private -Enabled true
+            New-NetFirewallRule -DisplayName "Allow NTP" -Direction Outbound -Protocol UDP -RemotePort 123 -Action Allow -Profile Domain,Public,Private -Enabled true
+            New-NetFirewallRule -DisplayName "Allow SMB" -Direction Outbound -Protocol TCP -RemotePort 445 -Action Allow -Profile Domain,Public,Private -Enabled true
+            }
+        catch {
+            Write-Host "4th Stage Failed"
+        }
     }
+
 
 
 LandOnTat
