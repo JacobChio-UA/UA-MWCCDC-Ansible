955b80f8b2de066ab51c7273f9a13398926e580c
Jacob Chio <jlc396@uakron.edu>
2026-02-12 15:57:20 -0800

    even more

--- windows/tieFighter.ps1
+++ windows/tieFighter.ps1
@@ -1,5 +1,5 @@
-wget 'https://www.openinfosecfoundation.org/download/windows/Suricata-8.0.3-windivert-1-64bit.msi' -O 'suricata.msi' -UseBasicParsing
+curl.exe -OL 'https://www.openinfosecfoundation.org/download/windows/Suricata-8.0.3-windivert-1-64bit.msi' --output 'suricata.msi'
 Start-Process msiexec.exe -Wait -ArgumentList '/I suricata.msi /qn'
-(Invoke-WebRequest 'https://rules.emergingthreats.net/open/suricata-8.0.3/rules/' -UseBasicParsing).Links | Where-Object -Property href -match '.rules' | ForEach-Object {
+(Invoke-WebRequest 'https://rules.emergingthreats.net/open/suricata-8.0.3/rules/' -UseBasicParsing).Links | Where-Object -Property href -match '.rules' | ForEach-Object -parallel {
     curl.exe -OL "https://rules.emergingthreats.net/open/suricata-8.0.3/rules$($_.href.substring(1))" --output "C:\Program Files\Suricata\rules\$($_.href.substring(2))"
-}
+} 
