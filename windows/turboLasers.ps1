e4d41d1f0eba03af307959e2144fd6ad0dddc8fa
Jacob Chio <jlc396@uakron.edu>
2026-02-12 15:57:13 -0800

    new stuff

--- windows/turboLasers.ps1
+++ windows/turboLasers.ps1
@@ -3,4 +3,4 @@ Install-Module -Name PSWindowsUpdate -Force
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Scope Process
 Import-Module PSWindowsUpdate
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false -Silent
-Get-WindowsUpdate -AcceptAll -Install -Category 'Security Updates' -IgnoreReboot -IgnoreUserInput -IgnoreRebootRequired
+Get-WindowsUpdate -AcceptAll -Install -Category 'Security Updates' -IgnoreReboot
