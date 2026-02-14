4065585042d48922988939a611235f915b58418a
Jacob Chio <jlc396@uakron.edu>
2026-02-13 20:48:54 -0800

    fix

--- windows/00-cargoship.ps1
+++ windows/00-cargoship.ps1
@@ -6,7 +6,7 @@ curl.exe -OL 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
 Expand-Archive -Path SysinternalsSuite.zip -DestinationPath C:\SysinternalsSuite\ -Force
 winget.exe install WiresharkFoundation.Wireshark -e --accept-source-agreements --accept-package-agreements
 winget.exe install Insecure.Nmap -e --accept-source-agreements --accept-package-agreements
-winget.exe Microsoft.PowerShell -e --accept-source-agreements --accept-package-agreements
+winget.exe install Microsoft.PowerShell -e --accept-source-agreements --accept-package-agreements
 winget.exe install Microsoft.VisualStudioCode -e --accept-source-agreements --accept-package-agreements
 winget.exe install Microsoft.WindowsTerminal -e --accept-source-agreements --accept-package-agreements
 winget.exe install Microsoft.etl2pcapng -e --accept-source-agreements --accept-package-agreements
