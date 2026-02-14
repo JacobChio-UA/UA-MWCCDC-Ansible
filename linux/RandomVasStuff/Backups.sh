b13211aa3d92aecb0b6132a39340ba53e7ab4ed0
Agent-raptor524 <raptorgaming524@gmail.com>
2026-02-13 21:59:08 -0500

    back?

---
+++ linux/RandomVasStuff/Backups.sh
@@ -0,0 +1,12 @@
+SDP = "/etc/postfix"
+DDP = "/mnt/backup/postfixB"
+Dir = "/mnt/backup"
+SYD = "/etc/sysconfig"
+DOV = "/etc/dovecot"
+#making a the directory and doing the stuff ig
+if [ ! -d "$dir" ]; then    
+    mkdir -p "$dir"
+fi
+cp -r "$SDP" "$DDP"
+cp -r "$syd" "/mnt/backup/sysconfigB"
+cp -r "$DOV" "/mnt/backup/dovecotB"
