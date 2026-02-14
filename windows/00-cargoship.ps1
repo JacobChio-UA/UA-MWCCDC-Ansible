da843765866fb0cff49cd4501df6f73bf7cf3fce
Jacob Chio <jlc396@uakron.edu>
2026-02-13 18:38:38 -0800

    update

--- windows/00-cargoship.ps1
+++ windows/00-cargoship.ps1
@@ -3,12 +3,10 @@ winget-install.ps1
 winget install curl.curl -e --accept-source-agreements --accept-package-agreements
 curl.exe -OL 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
 Expand-Archive -Path SysinternalsSuite.zip -DestinationPath C:\SysinternalsSuite\
-new-shortcut -target "C:\SysinternalsSuite\PsExec.exe" -shortcutpath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\PsExec.lnk"
 winget install WiresharkFoundation.Wireshark -e --accept-source-agreements --accept-package-agreements
 winget install Insecure.Nmap -e --accept-source-agreements --accept-package-agreements
 winget install Microsoft.PowerShell -e --accept-source-agreements --accept-package-agreements
 winget install Microsoft.VisualStudioCode -e --accept-source-agreements --accept-package-agreements
 winget install Microsoft.WindowsTerminal -e --accept-source-agreements --accept-package-agreements
 winget install Microsoft.etl2pcapng -e --accept-source-agreements --accept-package-agreements
-winget install git.git -e --accept-source-agreements --accept-package-agreements
-
+winget install Git.Git -e --accept-source-agreements --accept-package-agreements
