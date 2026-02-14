6e9a55c5df6dc4d159c957cf153c0f432d3a32e7
Agent-raptor524 <raptorgaming524@gmail.com>
2026-02-14 18:37:54 -0500

    okokokok trust. this time red team, it will work as I intend and wont explod catastrophically. . .

--- linux/RandomVasStuff/noroot.sh
+++ linux/RandomVasStuff/noroot.sh
@@ -11,7 +11,9 @@ else
 fi
 gpasswd -d "$user" wheel
 
-if [$? -eq 0 ]; then
+
+#removing from wheel.
+if [ $? -eq 0 ]; then
   echo "User '$user' hasthd been oblitorated."
 else
   echo "Failed to remove user '$user' from wheel group. Exiting."
@@ -20,7 +22,65 @@ fi
 #verify user does not have sudo access 
 if sudo -lU "$user" >/dev/null 2>&1; then
   echo "User '$user' guess it didn't work still has sudo."
+  exit 1
 else
   echo "User '$user' L + ratio the job is done."
 fi
+
+
+#lock account
+read -p "do you want to remove login for $user y/n: " wack
+if [ "$wack" == "y" ]; then
+  usermod -L "$user"
+  if [ $? -eq 0 ]; then
+    echo "User '$user' has been locked."
+  else
+    echo "Failed to lock user '$user'. Exiting."
+  fi
+else
+  echo "User '$user' I see how it is."
+fi
+
+
+#kill sessions
+read -p "blow up sessions y/n: " esplode
+if [ "$esplode" == "y" ]; then
+  pkill -u "$user"
+  if [ $? -eq 0 ]; then
+    echo "All sessions for user '$user' have been terminated."
+  else
+    echo "Failed to terminate sessions for user '$user'. Exiting."
+  fi
+else
+  echo "User '$user' is still chilling."
+  
+fi
+
+
+#esplode cronjobs or smthn probs wont work
+crontab -u "$user" -l
+read -p "do you want to remove cron jobs for $user y/n: " cron
+if [ "$cron" == "y" ]; then
+    crontab -u "$user" -r
+    if [ $? -eq 0 ]; then
+        echo "Cron jobs for user '$user' have been removed."
+    else
+        echo "Failed to remove cron jobs for user '$user'."
+    fi
+else
+    echo "didn't do anythin."
+
+echo "things im probably missing but cant care enough to add: "
+echo "groups"
+
+groups "$user"
+
+echo "checking lingering"
+ls /var/lib/systemd/linger
+
+echo "checking sudoers files"
+grep -r "$user" /etc/sudoers /etc/sudoers.d 
+
+echo "checking for processes"
+ps -u "$user"
 
