b2e891a4eb13f01b8337db2dcb7b7c76722edfa9
Agent-raptor524 <raptorgaming524@gmail.com>
2026-02-13 22:05:55 -0500

    uggggghhhh

--- linux/RandomVasStuff/Backups.sh
+++ linux/RandomVasStuff/Backups.sh
@@ -1,8 +1,8 @@
-SDP = ("/etc/postfix")
-DDP = ("/mnt/backup/postfixB")
-Dir = ("/mnt/backup")
-SYD = ("/etc/sysconfig")
-DOV = ("/etc/dovecot")
+SDP= ("/etc/postfix")
+DDP= ("/mnt/backup/postfixB")
+Dir= ("/mnt/backup")
+SYD= ("/etc/sysconfig")
+DOV= ("/etc/dovecot")
 #making a the directory and doing the stuff ig
 if [ ! -d "$dir" ]; then    
     mkdir -p "$dir"
