02fff4e913abfc2306324ff1d7e7a57dbe3adc6a
Agent-raptor524 <raptorgaming524@gmail.com>
2026-02-13 22:15:11 -0500

    and again. . .

--- linux/RandomVasStuff/Backups.sh
+++ linux/RandomVasStuff/Backups.sh
@@ -3,11 +3,13 @@ DDP=("/mnt/backup/postfixB")
 Dir=("/mnt/backup")
 SYD=("/etc/sysconfig")
 DOV=("/etc/dovecot")
+SCB=("/mnt/backup/sysconfigB")
+DOVB=("/mnt/backup/dovecotB")
 #making a the directory and doing the stuff ig
 if [ ! -d "$Dir" ]; then    
     mkdir -p "$Dir"
 fi
 cp -r "$SDP" "$DDP"
-cp -r "$syd" "/mnt/backup/sysconfigB"
-cp -r "$DOV" "/mnt/backup/dovecotB"
+cp -r "$syd" "$SCB"
+cp -r "$DOV" "$DOVB"
 
