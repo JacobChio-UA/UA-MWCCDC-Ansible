a59f5f36eb073238ecf3d3f63ce7e8df13a51db0
Jake-Charlton <jc481@uakron.edu>
2026-02-13 20:57:07 -0500

    deathStar

---
+++ windows/deathStar.ps1
@@ -0,0 +1,126 @@
+function deathStar() {
+    # Prompt user for server selection
+    Write-Host "Select a server type: AD (1), HTTP (2), FTP (3)" 
+    
+    $serverChoice = Read-Host "Enter your 1, 2, or 3"
+    
+        switch ($serverChoice) {
+            "1" {
+                # AD STUFF HERE
+            }
+            "2" {
+                try {
+                    # Enable Windows Firewall for all profiles
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled true
+
+                    # Set default inbound policy to Block
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -DefaultInboundAction Block -DefaultOutboundAction Block
+
+                    # Enable logging for packets
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -LogAllowed true -LogBlocked true
+
+                    # Disallow Configuration Changes
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -AllowLocalPolicyMerge false -AllowLocalIPsecPolicyMerge false -AllowLocalFirewallRules false -AllowLocalIPsecRules false
+
+                    Write-Host "No Errors in 1st Stage, Continue" 
+                }
+                catch {
+                    Write-Host "1st Stage Failed"
+                }
+                try {
+                    # Begin Allowing Inbound Scoring
+                    New-NetFirewallRule -DisplayName "Allow HTTP" -Direction Inbound -Protocol TCP -LocalPort 80 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow HTTPS" -Direction Inbound -Protocol TCP -LocalPort 443 -Action Allow -Profile Domain,Public,Private -Enabled true
+
+                    # Remove Preexisting Inbound Rules
+                    Get-NetFirewallRule -Direction Inbound | Remove-NetFirewallRule
+
+                    Write-Host "No Errors in 2nd Stage, Continue"
+                }
+                catch {
+                    Write-Host "2nd Stage Failed"
+                }
+                try {
+                    # Allow Communication with DNS and AD 
+                    New-NetFirewallRule -DisplayName "Allow DNS" -Direction Outbound -Protocol UDP -RemotePort 53 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow LDAP" -Direction Outbound -Protocol TCP -RemotePort 389,636,3268,3269 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow Kerberos" -Direction Outbound -Protocol TCP -RemotePort 88 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    
+                    Write-Host "No Errors in 3rd Stage, Continue"
+                }  
+                catch {
+                    Write-Host "3rd Stage Failed"
+                }
+                try {
+                    # Allow Necessary Connections
+                    Enable-NetFirewallRule -DisplayGroup "Core Networking"
+                    New-NetFirewallRule -DisplayName "Allow NTP" -Direction Outbound -Protocol UDP -RemotePort 123 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow SMB" -Direction Outbound -Protocol TCP -RemotePort 445 -Action Allow -Profile Domain,Public,Private -Enabled true
+                }
+                catch {
+                    Write-Host "4th Stage Failed"
+                }
+            }
+            "3" {
+                try {
+                    # Enable Windows Firewall for all profiles
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled true
+
+                    # Set default inbound policy to Block
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -DefaultInboundAction Block -DefaultOutboundAction Block
+
+                    # Enable logging for packets
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -LogAllowed true -LogBlocked true
+
+                    # Disallow Configuration Changes
+                    Set-NetFirewallProfile -Profile Domain,Public,Private -AllowLocalPolicyMerge false -AllowLocalIPsecPolicyMerge false -AllowLocalFirewallRules false -AllowLocalIPsecRules false
+
+                    Write-Host "No Errors in 1st Stage, Continue" 
+                }
+                catch {
+                    Write-Host "1st Stage Failed"
+                }
+                try {
+                    # Begin Allowing Inbound Scoring
+                    New-NetFirewallRule -DisplayName "Allow FTP" -Direction Inbound -Protocol TCP -LocalPort 20,21 -Action Allow -Profile Domain,Public,Private -Enabled true
+
+                    # Remove Preexisting Inbound Rules
+                    Get-NetFirewallRule -Direction Inbound | Remove-NetFirewallRule
+
+                    Write-Host "No Errors in 2nd Stage, Continue"
+                }
+                catch {
+                    Write-Host "2nd Stage Failed"
+                }
+                try { 
+                    # Allow Communication with DNS and AD 
+                    New-NetFirewallRule -DisplayName "Allow DNS" -Direction Outbound -Protocol UDP -RemotePort 53 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow LDAP" -Direction Outbound -Protocol TCP -RemotePort 389,636,3268,3269 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow Kerberos" -Direction Outbound -Protocol TCP -RemotePort 88 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    
+                    Write-Host "No Errors in 3rd Stage, Continue"
+                }  
+                catch {
+                    Write-Host "3rd Stage Failed"
+                }
+                try {
+                    # Allow Necessary Connections
+                    Enable-NetFirewallRule -DisplayGroup "Core Networking"
+                    New-NetFirewallRule -DisplayName "Allow NTP" -Direction Outbound -Protocol UDP -RemotePort 123 -Action Allow -Profile Domain,Public,Private -Enabled true
+                    New-NetFirewallRule -DisplayName "Allow SMB" -Direction Outbound -Protocol TCP -RemotePort 445 -Action Allow -Profile Domain,Public,Private -Enabled true
+                }
+                catch {
+                    Write-Host "4th Stage Failed"
+                }
+            }
+            default {
+                Write-Host "Invalid selection. Please enter 1, 2, or 3."
+                return
+            }
+        }
+    }
+
+
+
+deathStar
+
