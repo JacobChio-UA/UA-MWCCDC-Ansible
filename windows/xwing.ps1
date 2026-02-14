9dde2ebb72bf29d757a6f895632bd96c17481ede
Jacob Chio <jlc396@uakron.edu>
2026-02-13 22:10:45 -0800

    beef

--- windows/xwing.ps1
+++ windows/xwing.ps1
@@ -1,11 +1,16 @@
 curl.exe -o 'Windows Server 2022 Security Baseline.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/Windows%20Server%202022%20Security%20Baseline.zip'
 Expand-Archive -Path 'Windows Server 2022 Security Baseline.zip' -DestinationPath 'C:\Security Baseline' -force 
 curl.exe -o 'Windows 10 Version 1809 and Windows Server 2019 Security Baseline.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/Windows%2010%20Version%201809%20and%20Windows%20Server%202019%20Security%20Baseline.zip' 
-Expand-Archive -Path 'Windows 10 Version 1809 and Windows Server 2019 Security Baseline.zip' -DestinationPath 'C:\Security Baseline\WinServer2019\' -force
+Expand-Archive -Path 'Windows 10 Version 1809 and Windows Server 2019 Security Baseline.zip' -DestinationPath 'C:\Security Baseline\WinServer2019\' -force 
 curl.exe -o 'Windows 11 v25H2 Security Baseline.zip' 'https://download.microsoft.com/download/e99be2d2-e077-4986-a06b-6078051999dd/Windows%2011%20v25H2%20Security%20Baseline.zip'
-Expand-Archive -Path 'Windows 11 v25H2 Security Baseline.zip' -DestinationPath 'C:\Security Baseline\Win11\' -force
-curl.exe -o 'LGPO.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/LGPO.zip'
-Expand-Archive -Path 'LGPO.zip' -DestinationPath 'C:\Security Baseline\LGPO\' -force
+Expand-Archive -Path 'Windows 11 v25H2 Security Baseline.zip' -DestinationPath 'C:\Security Baseline' -force
+
+
+cd 'C:\Security Baseline\Windows 11 v25H2 Security Baseline\Scripts'
+.\Baseline-ADImport.ps1
+cd 'C:\Security Baseline\WinServer2019\Windows 10 Version 1809 and Windows Server 2019 Security Baseline\Scripts'
+.\Baseline-ADImport.ps1
+cd 'C:\Security Baseline\WinServer2022\Windows Server 2022 Security Baseline\Scripts'
 
 try {
     new-adorganizationalunit -name "Win11Workstation"
