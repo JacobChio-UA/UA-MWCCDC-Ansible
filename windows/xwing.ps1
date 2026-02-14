9759a00c7b5bed626a21916e82ae41264f93d9cf
Jacob Chio <jlc396@uakron.edu>
2026-02-13 21:26:08 -0800

    ope

--- windows/xwing.ps1
+++ windows/xwing.ps1
@@ -1,11 +1,11 @@
 curl.exe -o 'Windows Server 2022 Security Baseline.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/Windows%20Server%202022%20Security%20Baseline.zip'
 Expand-Archive -Path 'Windows Server 2022 Security Baseline.zip' -DestinationPath 'C:\Security Baseline' -force 
-mv 'C:\Security Baseline\Windows Server-2022-Security-Baseline-Final\' 'C:\Security Baseline\' -Force
+mv  -Path 'C:\Security Baseline\Windows Server-2022-Security-Baseline-Final\' -Destination 'C:\Security Baseline\' -Force
 curl.exe -o 'Windows 10 Version 1809 and Windows Server 2019 Security Baseline.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/Windows%2010%20Version%201809%20and%20Windows%20Server%202019%20Security%20Baseline.zip' 
 Expand-Archive -Path 'Windows 10 Version 1809 and Windows Server 2019 Security Baseline.zip' -DestinationPath 'C:\Security Baseline' -force
 curl.exe -o 'Windows 11 v25H2 Security Baseline.zip' 'https://download.microsoft.com/download/e99be2d2-e077-4986-a06b-6078051999dd/Windows%2011%20v25H2%20Security%20Baseline.zip'
 Expand-Archive -Path 'Windows 11 v25H2 Security Baseline.zip' -DestinationPath 'C:\Security Baseline' -force
-mv 'C:\Security Baseline\Windows 11 v25H2 Security Baseline' 'C:\Security Baseline\' -Force
+mv -Path 'C:\Security Baseline\Windows 11 v25H2 Security Baseline' -Destination 'C:\Security Baseline\' -Force
 curl.exe -o 'LGPO.zip' 'https://download.microsoft.com/download/8/5/c/85c25433-a1b0-4ffa-9429-7e023e7da8d8/LGPO.zip'
 Expand-Archive -Path 'LGPO.zip' -DestinationPath 'C:\Security Baseline' -force
 & 'C:\Security Baseline\LGPO_30\LGPO.exe'
