d52fbda858546ee2b16571a900e84f0d9f4c3136
Jacob Chio <jlc396@uakron.edu>
2026-02-13 20:31:05 -0800

    Fixes

--- windows/00-cargoship.ps1
+++ windows/00-cargoship.ps1
@@ -1,12 +1,12 @@
-Install-Script -name winget-install -force
+Save-Script -name winget-install -path
 winget-install.ps1
-winget install curl.curl -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install curl.curl -e --accept-source-agreements --accept-package-agreements
 curl.exe -OL 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
 Expand-Archive -Path SysinternalsSuite.zip -DestinationPath C:\SysinternalsSuite\
-winget install WiresharkFoundation.Wireshark -e --accept-source-agreements --accept-package-agreements
-winget install Insecure.Nmap -e --accept-source-agreements --accept-package-agreements
-winget install Microsoft.PowerShell -e --accept-source-agreements --accept-package-agreements
-winget install Microsoft.VisualStudioCode -e --accept-source-agreements --accept-package-agreements
-winget install Microsoft.WindowsTerminal -e --accept-source-agreements --accept-package-agreements
-winget install Microsoft.etl2pcapng -e --accept-source-agreements --accept-package-agreements
-winget install Git.Git -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install WiresharkFoundation.Wireshark -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install Insecure.Nmap -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe Microsoft.PowerShell -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install Microsoft.VisualStudioCode -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install Microsoft.WindowsTerminal -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install Microsoft.etl2pcapng -e --accept-source-agreements --accept-package-agreements
+%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe install Git.Git -e --accept-source-agreements --accept-package-agreements
